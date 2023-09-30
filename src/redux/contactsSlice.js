98fd6ea0f9d03efff6ce5b8b31e58cb7ec03a2ac
Volodymyr <vladikon70@gmail.com>
2023-09-30 20:24:45 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -42,3 +42,30 @@ export const delContact = idContact => {
     payload: idContact,
   };
 };
+
+// ================ Для redux/toolkit==========================
+
+// export const addContact = createAction('contacts/addContact');
+
+// export const delContact = createAction('contacts/delContact');
+
+// export const contactsReducer = createReducer(initialState, builder =>
+//   builder
+//     .addCase(addContact, (state, action) => {
+//       const isDuplicated = state.contacts.find(
+//         item => item.name.toLowerCase() === action.payload.name.toLowerCase()
+//       );
+//       if (isDuplicated)
+//         return alert(action.payload.name + ' is already in contacts');
+//       return {
+//         contacts: [...state.contacts, { id: nanoid(4), ...action.payload }],
+//       };
+//     })
+//     .addCase(delContact, (state, action) => {
+//       if (window.confirm('Are you sure?'))
+//         return {
+//           contacts: state.contacts.filter(({ id }) => id !== action.payload),
+//         };
+//       return;
+//     })
+// );
